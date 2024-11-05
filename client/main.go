7385daf8e324463c9d9a5e17298982f75466cde9
Patrick Akil <pakil@xebia.com>
2024-11-05 12:37:05 +0100

    [CLIENT] starts with a blank client

--- client/main.go
+++ client/main.go
@@ -1,59 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	cliArgs := parseArgs()
 
-	log.Printf("args: %+v", cliArgs)
+	// TODO: create http client and send requests
 
-	// create http client and send requests
-
-}
-
-type args struct {
-	command    string
-	email      string
-	name       string
-	bsn        int
-	pinCode    int
-	patientUID string
-}
-
-func parseArgs() args {
-
-	help := flag.Bool("help", false, "This help text")
-	command := flag.String("command", "start-registration", "Command (start-registration, complete-registration or brute-force-registration)")
-
-	bsn := flag.Int("bsn", 12345, "Bsn number of patient")
-	name := flag.String("name", "Michael Jordan", "Name of patient")
-	email := flag.String("email", "eva.marc@hetnet.nl", "Email address of patient")
-
-	patientUID := flag.String("patient-uid", "", "Uid of patient")
-	pinCode := flag.Int("pin-code", -1, "Pincode to confirm registration")
-
-	flag.Parse()
-
-	if help != nil && *help {
-		fmt.Fprintf(os.Stderr, "\nUsage:\n")
-		fmt.Fprintf(os.Stderr, "\t[flags]\n")
-		flag.PrintDefaults()
-		fmt.Fprintf(os.Stderr, "\n")
-		os.Exit(-1)
-	}
-
-	return args{
-		command: *command,
-		email:   *email,
-		name:    *name,
-		bsn:     *bsn,
-
-		pinCode:    *pinCode,
-		patientUID: *patientUID,
-	}
 }
